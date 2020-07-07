b9788e2a56ce97d3505c0bc7caa899beccdc7d63
Jeff Balboni <jeff@adhocteam.us>
2020-07-07 14:08:59 -0400

    WIP

--- src/applications/vaos/tests/mocks/helpers.js
+++ src/applications/vaos/tests/mocks/helpers.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import environment from 'platform/utilities/environment';
 import { mockFetch, setFetchJSONResponse } from 'platform/testing/unit/helpers';
+import { getVAAppointmentMock } from '../mocks/v0';
 
 export function mockAppointmentInfo({ va = [], cc = [], requests = [] }) {
   mockFetch();
@@ -66,5 +67,130 @@ export function mockFacilitesFetch(ids, facilities) {
   setFetchJSONResponse(
     global.fetch.withArgs(`${environment.API_URL}/v1/facilities/va?ids=${ids}`),
     { data: facilities },
+  );
+}
+
+export function mockEligibilityFetches({
+  siteId,
+  facilityId,
+  typeOfCareId,
+  limit = false,
+  requestPastVisits = false,
+  directPastVisits = false,
+  clinics = [],
+  pastClinics = false,
+}) {
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${
+        environment.API_URL
+      }/vaos/v0/facilities/${facilityId}/limits?type_of_care_id=${typeOfCareId}`,
+    ),
+    {
+      data: {
+        attributes: {
+          requestLimit: 1,
+          numberOfRequests: limit ? 0 : 1,
+        },
+      },
+    },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${
+        environment.API_URL
+      }/vaos/v0/facilities/${facilityId}/visits/direct?system_id=${siteId}&type_of_care_id=${typeOfCareId}`,
+    ),
+    {
+      data: {
+        attributes: {
+          durationInMonths: 12,
+          hasVisitedInPastMonths: directPastVisits,
+        },
+      },
+    },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${
+        environment.API_URL
+      }/vaos/v0/facilities/${facilityId}/visits/request?system_id=${siteId}&type_of_care_id=${typeOfCareId}`,
+    ),
+    {
+      data: {
+        attributes: {
+          durationInMonths: 12,
+          hasVisitedInPastMonths: requestPastVisits,
+        },
+      },
+    },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${
+        environment.API_URL
+      }/vaos/v0/facilities/${facilityId}/clinics?type_of_care_id=${typeOfCareId}&system_id=${siteId}`,
+    ),
+    {
+      data: clinics,
+    },
+  );
+
+  const appointment = getVAAppointmentMock();
+  appointment.attributes = {
+    ...appointment.attributes,
+    startDate: moment().format(),
+    clinicFriendlyName: 'C&P BEV AUDIO FTC1',
+    facilityId: '983',
+    sta6aid: '983GC',
+    clinicId: clinics?.[0].id,
+  };
+  appointment.attributes.vdsAppointments[0].currentStatus = 'FUTURE';
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${environment.API_URL}/vaos/v0/appointments?start_date=${moment()
+        .startOf('day')
+        .subtract(6, 'months')
+        .toISOString()}&end_date=${moment()
+        .startOf('day')
+        .toISOString()}&type=va`,
+    ),
+    { data: pastClinics && clinics.length ? [appointment] : [] },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${environment.API_URL}/vaos/v0/appointments?start_date=${moment()
+        .startOf('day')
+        .subtract(12, 'months')
+        .toISOString()}&end_date=${moment()
+        .startOf('day')
+        .subtract(6, 'months')
+        .toISOString()}&type=va`,
+    ),
+    { data: [] },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${environment.API_URL}/vaos/v0/appointments?start_date=${moment()
+        .startOf('day')
+        .subtract(18, 'months')
+        .toISOString()}&end_date=${moment()
+        .startOf('day')
+        .subtract(12, 'months')
+        .toISOString()}&type=va`,
+    ),
+    { data: [] },
+  );
+  setFetchJSONResponse(
+    global.fetch.withArgs(
+      `${environment.API_URL}/vaos/v0/appointments?start_date=${moment()
+        .startOf('day')
+        .subtract(24, 'months')
+        .toISOString()}&end_date=${moment()
+        .startOf('day')
+        .subtract(18, 'months')
+        .toISOString()}&type=va`,
+    ),
+    { data: [] },
   );
 }
