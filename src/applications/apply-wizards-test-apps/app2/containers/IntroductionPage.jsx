49b680cad643e0ff4b2ea444facc10c796fbf289
mr0sari0 <mrosario@governmentcio.com>
2020-07-10 03:15:55 -0400

    fix conditionals

--- src/applications/apply-wizards-test-apps/app2/containers/IntroductionPage.jsx
+++ src/applications/apply-wizards-test-apps/app2/containers/IntroductionPage.jsx
@@ -32,6 +32,8 @@ class IntroductionPage extends React.Component {
           localStorage.getItem('educationBenefitSelected') || 'none selected',
         wizardCompletionStatus:
           localStorage.getItem('EduWizardStatus') || 'not complete',
+        hasUserLeftLandingPage:
+          localStorage.getItem('hasUserLeftLandingPage') || 'false',
       });
   }
 
@@ -99,6 +101,12 @@ class IntroductionPage extends React.Component {
         className="usa-button va-button-primary"
         onClick={() => {
           this.recordWizardValues();
+          localStorage.setItem('hasUserLeftLandingPage', 'true');
+          this.setState({
+            userHasLeftLandingPage: localStorage.getItem(
+              'hasUserLeftLandingPage',
+            ),
+          });
         }}
       >
         Apply now
@@ -217,6 +225,7 @@ class IntroductionPage extends React.Component {
       open,
       wizardCompletionStatus,
       educationBenefitSelected,
+      hasUserLeftLandingPage,
     } = this.state;
 
     const buttonClasses = classNames('usa-button-primary', 'wizard-button', {
@@ -249,11 +258,15 @@ class IntroductionPage extends React.Component {
         value: 'extend',
       },
     ];
+    const shouldWizardBeRendered = hasUserLeftLandingPage === 'false';
+
+    const shouldIntroPageBeRendered = hasUserLeftLandingPage === 'true';
+
     return (
       <div>
         <FormTitle title="Apply Wizards Test App 2" />
         <p>Equal to VA Form (Apply Wizards Test App 2).</p>
-        {educationBenefitSelected !== 'app2' && (
+        {shouldWizardBeRendered && (
           <div className="wizard-container">
             <h3>
               Let's find out which education benefits form would suit you the
@@ -544,76 +557,72 @@ class IntroductionPage extends React.Component {
             )}
           </div>
         )}
-        {educationBenefitSelected === 'app2' &&
-          wizardCompletionStatus === 'complete' && (
-            <div className="schemaform-intro">
-              <SaveInProgressIntro
-                prefillEnabled={this.props.route.formConfig.prefillEnabled}
-                messages={this.props.route.formConfig.savedFormMessages}
-                pageList={this.props.route.pageList}
-                startText="Start the Application"
-              >
-                Please complete the form to apply for benefits.
-              </SaveInProgressIntro>
-              <h4>Follow the steps below to apply for benefits.</h4>
-              <div className="process schemaform-process">
-                <ol>
-                  <li className="process-step list-one">
-                    <h5>Prepare</h5>
-                    <h6>To fill out this application, you’ll need your:</h6>
-                    <ul>
-                      <li>Social Security number (required)</li>
-                    </ul>
-                    <p>
-                      <strong>
-                        What if I need help filling out my application?
-                      </strong>{' '}
-                      An accredited representative, like a Veterans Service
-                      Officer (VSO), can help you fill out your claim.{' '}
-                      <a href="/disability-benefits/apply/help/index.html">
-                        Get help filing your claim
-                      </a>
-                    </p>
-                  </li>
-                  <li className="process-step list-two">
-                    <h5>Apply</h5>
-                    <p>Complete this benefits form.</p>
-                    <p>
-                      After submitting the form, you’ll get a confirmation
-                      message. You can print this for your records.
-                    </p>
-                  </li>
-                  <li className="process-step list-three">
-                    <h5>VA Review</h5>
-                    <p>
-                      We process claims within a week. If more than a week has
-                      passed since you submitted your application and you
-                      haven’t heard back, please don’t apply again. Call us at.
-                    </p>
-                  </li>
-                  <li className="process-step list-four">
-                    <h5>Decision</h5>
-                    <p>
-                      Once we’ve processed your claim, you’ll get a notice in
-                      the mail with our decision.
-                    </p>
-                  </li>
-                </ol>
-              </div>
-              <SaveInProgressIntro
-                buttonOnly
-                messages={this.props.route.formConfig.savedFormMessages}
-                pageList={this.props.route.pageList}
-                startText="Start the Application"
-              />
-              <div
-                className="omb-info--container"
-                style={{ paddingLeft: '0px' }}
-              >
-                <OMBInfo resBurden={30} ombNumber="" expDate="12/31/2021" />
-              </div>
+        {shouldIntroPageBeRendered && (
+          <div className="schemaform-intro">
+            <SaveInProgressIntro
+              prefillEnabled={this.props.route.formConfig.prefillEnabled}
+              messages={this.props.route.formConfig.savedFormMessages}
+              pageList={this.props.route.pageList}
+              startText="Start the Application"
+            >
+              Please complete the form to apply for benefits.
+            </SaveInProgressIntro>
+            <h4>Follow the steps below to apply for benefits.</h4>
+            <div className="process schemaform-process">
+              <ol>
+                <li className="process-step list-one">
+                  <h5>Prepare</h5>
+                  <h6>To fill out this application, you’ll need your:</h6>
+                  <ul>
+                    <li>Social Security number (required)</li>
+                  </ul>
+                  <p>
+                    <strong>
+                      What if I need help filling out my application?
+                    </strong>{' '}
+                    An accredited representative, like a Veterans Service
+                    Officer (VSO), can help you fill out your claim.{' '}
+                    <a href="/disability-benefits/apply/help/index.html">
+                      Get help filing your claim
+                    </a>
+                  </p>
+                </li>
+                <li className="process-step list-two">
+                  <h5>Apply</h5>
+                  <p>Complete this benefits form.</p>
+                  <p>
+                    After submitting the form, you’ll get a confirmation
+                    message. You can print this for your records.
+                  </p>
+                </li>
+                <li className="process-step list-three">
+                  <h5>VA Review</h5>
+                  <p>
+                    We process claims within a week. If more than a week has
+                    passed since you submitted your application and you haven’t
+                    heard back, please don’t apply again. Call us at.
+                  </p>
+                </li>
+                <li className="process-step list-four">
+                  <h5>Decision</h5>
+                  <p>
+                    Once we’ve processed your claim, you’ll get a notice in the
+                    mail with our decision.
+                  </p>
+                </li>
+              </ol>
             </div>
-          )}
+            <SaveInProgressIntro
+              buttonOnly
+              messages={this.props.route.formConfig.savedFormMessages}
+              pageList={this.props.route.pageList}
+              startText="Start the Application"
+            />
+            <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
+              <OMBInfo resBurden={30} ombNumber="" expDate="12/31/2021" />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
