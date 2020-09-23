3d333160bfc38cb2a63f19ed4324ea02ee56d21b
Brooks Johnson <brooks.johnson@pm.me>
2020-09-23 14:49:19 -0700

    Remove the Cypress axe exception from hca

--- src/applications/hca/tests/hca.cypress.spec.js
+++ src/applications/hca/tests/hca.cypress.spec.js
@@ -8,7 +8,6 @@ import manifest from '../manifest.json';
 
 const testConfig = createTestConfig(
   {
-    _13647Exception: true,
     dataPrefix: 'data',
     dataSets: ['minimal-test', 'maximal-test', 'foreign-address-test'],
     fixtures: { data: path.join(__dirname, 'schema') },
