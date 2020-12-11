a0b515e9947a1720d4252cb1fb85b624acf3a846
Joe <elder_joseph@bah.com>
2020-12-11 11:46:03 -0500

    16729: Styling based off UX feedback; unit test update

--- src/applications/edu-benefits/1990/tests/containers/ConfirmationPage.unit.spec.jsx
+++ src/applications/edu-benefits/1990/tests/containers/ConfirmationPage.unit.spec.jsx
@@ -26,7 +26,7 @@ describe('Edu 1990 <ConfirmationPage>', () => {
     const tree = SkinDeep.shallowRender(<ConfirmationPage form={form} />);
 
     expect(tree.subTree('.confirmation-page-title').text()).to.equal(
-      'We’ve received your application',
+      'We’ve received your application.',
     );
     expect(
       tree
