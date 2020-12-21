aac9c0b006e50c048cd2a2bbf109f2463e60706f
Sandra Hallie <sandra.adamshallie@gmail.com>
2020-12-21 14:47:28 -0700

    Move logic out of higher level components

--- src/applications/personalization/profile/components/personal-information/email-addresses/EmailField.jsx
+++ src/applications/personalization/profile/components/personal-information/email-addresses/EmailField.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  emailUiSchema,
+  emailFormSchema,
+  emailConvertDataToPayload,
+} from 'applications/personalization/profile/util/emailUtils';
 
 import { API_ROUTES, FIELD_NAMES } from '@@vap-svc/constants';
 
 import ContactInformationField from '../ContactInformationField';
-
-const formSchema = {
-  type: 'object',
-  properties: {
-    emailAddress: {
-      type: 'string',
-      // This regex was taken from the HCA but modified to allow leading and
-      // trailing whitespace to reduce false errors. The `convertDataToPayload`
-      // method will clean up the whitespace before submission
-      pattern:
-        '^(\\s)*(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))(\\s)*$',
-    },
-  },
-  required: ['emailAddress'],
-};
-const uiSchema = {
-  emailAddress: {
-    'ui:title': 'Email Address',
-    'ui:errorMessages': {
-      required: 'Please enter your email address, using this format: X@X.com',
-      pattern:
-        'Please enter your email address again, using this format: X@X.com',
-    },
-  },
-};
 
 export default class EmailField extends React.Component {
   static propTypes = {
@@ -36,19 +16,15 @@ export default class EmailField extends React.Component {
     fieldName: PropTypes.oneOf([FIELD_NAMES.EMAIL]).isRequired,
   };
 
-  convertDataToPayload(value) {
-    return { ...value, emailAddress: value.emailAddress.trim() };
-  }
-
   render() {
     return (
       <ContactInformationField
         title={this.props.title}
         fieldName={this.props.fieldName}
         apiRoute={API_ROUTES.EMAILS}
-        convertCleanDataToPayload={this.convertDataToPayload}
-        formSchema={formSchema}
-        uiSchema={uiSchema}
+        convertCleanDataToPayload={emailConvertDataToPayload}
+        formSchema={emailFormSchema}
+        uiSchema={emailUiSchema}
         type="email"
       />
     );
