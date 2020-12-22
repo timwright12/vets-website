45a244fffaad17789d7987de9ff3f5db21583c43
Shefali Nayak <shefali.nayak@thoughtworks.com>
2020-12-22 11:29:35 -0500

    replaces save in progress intro with form start controls

--- src/applications/ask-a-question/form/introduction/IntroductionPage.jsx
+++ src/applications/ask-a-question/form/introduction/IntroductionPage.jsx
@@ -5,6 +5,7 @@ import OMBInfo from '@department-of-veterans-affairs/formation-react/OMBInfo';
 import FormTitle from 'platform/forms-system/src/js/components/FormTitle';
 import SaveInProgressIntro from 'platform/forms/save-in-progress/SaveInProgressIntro';
 import { unauthStartText } from '../../constants/labels';
+import FormStartControls from "../../../../platform/forms/save-in-progress/FormStartControls";
 
 class IntroductionPage extends React.Component {
   componentDidMount() {
@@ -16,17 +17,15 @@ class IntroductionPage extends React.Component {
       <div className="schemaform-intro">
         <FormTitle title="Contact Us" />
         <p>Equal to VA Form 0873 (Ask a Question).</p>
-        <SaveInProgressIntro
-          prefillEnabled={this.props.route.formConfig.prefillEnabled}
-          messages={this.props.route.formConfig.savedFormMessages}
-          pageList={this.props.route.pageList}
-          formConfig={{
-            customText: this.props.route.formConfig.customText,
-          }}
-          unauthStartText={unauthStartText}
-        >
-          Please complete the 0873 form to send a message.
-        </SaveInProgressIntro>
+        <FormStartControls
+          startPage={'/topic'}
+          formId={this.props.route.formConfig.formId}
+          prefillTransformer={() => {}}
+          fetchInProgressForm={() => {}}
+          removeInProgressForm={() => {}}
+          prefillAvailable={false}
+          formSaved={false}
+        />
         <div className="omb-info--container" style={{ paddingLeft: '0px' }}>
           <OMBInfo resBurden={10} ombNumber="2900-0619" expDate="11/30/2019" />
         </div>
