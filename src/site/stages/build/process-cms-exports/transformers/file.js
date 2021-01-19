6eac281c793b44b0d6c07915ffaeca4063d24fbc
Bill Fienberg <billfienberg@gmail.com>
2021-01-19 17:18:27 -0600

    Add empty derivative transformation

--- src/site/stages/build/process-cms-exports/transformers/file.js
+++ src/site/stages/build/process-cms-exports/transformers/file.js
@@ -4,6 +4,7 @@ const transform = entity => ({
   // TODO: Change this to get the actual derivative from the CMS export data
   url: getDrupalValue(entity.uri) ? getDrupalValue(entity.uri) : '',
   filename: getDrupalValue(entity.filename),
+  derivative: {},
 });
 module.exports = {
   filter: ['uri', 'filename'],
