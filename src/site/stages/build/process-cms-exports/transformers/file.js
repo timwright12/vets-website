c4de9b211cddc61e5225f1dd468582797268a126
Chris Alexander <chris.alexander@adhocteam.us>
2021-01-20 16:44:08 -0700

    Replace public URL in file transformer

--- src/site/stages/build/process-cms-exports/transformers/file.js
+++ src/site/stages/build/process-cms-exports/transformers/file.js
@@ -1,8 +1,16 @@
 const { getDrupalValue } = require('./helpers');
 
+const formatUrl = originalUri => {
+  const drupalValue = getDrupalValue(originalUri);
+
+  return drupalValue
+    ? encodeURI(drupalValue.replace('public:/', '/files'))
+    : '';
+};
+
 const transform = entity => ({
   // TODO: Change this to get the actual derivative from the CMS export data
-  url: getDrupalValue(entity.uri) ? getDrupalValue(entity.uri) : '',
+  url: formatUrl(entity.uri),
   filename: getDrupalValue(entity.filename),
 });
 module.exports = {
